4941b2e076382754b0285782378e85ae432f5f47
Niklas Wolf <wolfniklas94@web.de>
2022-12-19 16:48:42 +0100

    Added optional xkeys-support

--- dist/module/module.cjs.js
+++ dist/module/module.cjs.js
@@ -9,19 +9,25 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
 
 var cacheModule = function () {
+    var options = this.options['nuxtShopwareCaching'];
     this['addPlugin']({
-        src: path__default["default"].resolve(__dirname, "plugin.js"),
+        src: path__default["default"].resolve(__dirname, 'plugin.js'),
         options: {
-            moduleName: "cache-tags"
+            moduleName: 'cache-tags'
         }
     });
-    this.nuxt.hook("render:route", function (url, _result, context) {
+    this.nuxt.hook('render:route', function (url, _result, context) {
         if (!context.res.headersSent) {
-            context.res.setHeader("X-Cache-Tags", context.$cacheTags.getCacheTags().join(","));
-            context.res.setHeader("X-Cache-Tags-Number", context.$cacheTags.getCacheTags().length);
+            if (options === null || options === void 0 ? void 0 : options.useXKey) {
+                context.res.setHeader('xkey', context.$cacheTags.getCacheTags().join(" "));
+            }
+            else {
+                context.res.setHeader('X-Cache-Tags', context.$cacheTags.getCacheTags().join(","));
+                context.res.setHeader('X-Cache-Tags-Number', context.$cacheTags.getCacheTags().length);
+            }
         }
         else {
-            console.error("Headers already sent:", context.res.statusCode + " " + url);
+            console.error('Headers already sent:', context.res.statusCode + ' ' + url);
         }
     });
 };
